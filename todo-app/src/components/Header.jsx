3aabe7cc86b930a80e0b908f7bf1813c4ebea0d3
LOVKUSH <lovkushyadav9888@gmail.com>
2023-02-05 02:08:56 +0530

    Header-component

---
+++ todo-app/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+        <h1>
+        <img src="https://img.icons8.com/external-flaticons-flat-flat-icons/1x/external-torch-prehistoric-flaticons-flat-flat-icons-2.png"></img>
+            TODO-List
+        </h1>
+    </header>
+  )
+}
+
+export default Header;
